ba558f4a2dd67407a77e364d4355c9f6d6ef8648
FAdy200 <abuyehia200@gmail.com>
2020-10-19 18:38:21 +0200

    stuff

---
+++ Hackerrank/the grid search.py
@@ -0,0 +1,21 @@
+cn = int(input())
+for _ in range(cn):
+    n, m = map(int, input().split())
+    x = []
+    for i in range(n):
+        x.append(list(map(int, input().split())))
+    loc = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
