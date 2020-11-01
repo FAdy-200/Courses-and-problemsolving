39c06f667aa9e46ff9d89e0f7981729f447f1aed
FAdy200 <abuyehia200@gmail.com>
2020-11-01 14:38:30 +0200

    need work

---
+++ Hackerrank/Ashton and String.py
@@ -0,0 +1,14 @@
+n = int(input())
+for _ in range(n):
+    x = input()
+    inq = int(input())
+    z = []
+    for i in range(len(x)):
+        for j in range(i+1, len(x)+1):
+            z.append(x[i:j])
+    z = sorted(z)
+    m = ("".join(z))
+    print(m)
+    print(m[inq-1])
+
+
